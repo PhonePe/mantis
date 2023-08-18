d4899a355f1ee558d8ed7d92e31ee061a4679170
Prateek Thakare <pratikthakare65@gmail.com>
2023-08-18 10:43:27 +0530

    Added verbose mode

--- mantis/config_parsers/logging_utils.py
+++ mantis/config_parsers/logging_utils.py
@@ -1,12 +1,19 @@
 import os
 import logging
 import logging.config
+from mantis.models.args_model import ArgsModel
 from mantis.config_parsers.config_client import ConfigProvider
 
 class LoggingConfig(object):
 
     @staticmethod    
-    def configure_logging():
-        log_config = ConfigProvider.get_config().logging
-        logging.config.dictConfig(log_config)
-        logging.info('MANTIS ASSET DISCOVERY - STARTED') 
+    def configure_logging(args: ArgsModel):
+        if args.verbose:
+            log_config = ConfigProvider.get_config().logging_debug
+            logging.config.dictConfig(log_config)
+            logging.info('MANTIS ASSET DISCOVERY - STARTED') 
+            logging.info("Debug mode enabled")
+        else:
+            log_config = ConfigProvider.get_config().logging
+            logging.config.dictConfig(log_config)
+            logging.info('MANTIS ASSET DISCOVERY - STARTED') 
