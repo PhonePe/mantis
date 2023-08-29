4e13d6cfae6f99c6a8392e6d890970f681e4208a
Bharath <9979523+0xbharath@users.noreply.github.com>
2023-08-29 15:32:43 +0530

    Updated the docker setup scripts

--- setup/docker/docker-setup-ubuntu.sh
+++ setup/docker/docker-setup-ubuntu.sh
@@ -48,13 +48,13 @@ MANDATORY PREREQUISITES
 
 Please ensure your system meets them before proceeding.
 
-1. Docker >= v19.0.0
-   - Check with 'docker --version'
-2. Docker compose >= v2.0.0
-   - Check with 'docker compose version'
-3. Docker configured to work without sudo 
+1. Install Docker >= v19.0.0
+   - https://docs.docker.com/engine/install/ubuntu/
+   - Check version with 'docker --version'
+2. Install Docker compose >= v2.0.0
+   - Check version with 'docker compose version'
 4. sudo access on the machine
-5. Port 1337 & 13337 available on host machine (for appsmith dashboard)
+5. Ports 1337, 1338, 27000 available on host machine (for Mantis dashboard and MongoDB)
     - If these ports can't be freed then modify the port mapping in docker-compose-mantis.yml 
       to any available ports
 ${NC}
@@ -72,20 +72,56 @@ else
 fi
 
 if docker compose ps | grep -q "Up"; then
-    echo -e -n "[?] ${BIYellow}Looks like this script was run previously to setup Mantis.
-    ${BICyan}Do you want the script to remove the resources created previously?
-    Selecting yes means, we will remove & recreate all the resources including MongoDB (y/n)? \n
-    ${BRed}(It is recommended to backup MongoDB before proceeding) ${NC}"
-    read remove_answer
-    
-    if [ "$remove_answer" != "${remove_answer#[Yy]}" ] ;then 
-        echo -e "[-] ${Red}Removing the existing containers${NC}"
-        docker compose down
-    else 
-        echo -e "[-] ${BYellow}Please clean up the resources from previous script manually \n
-                 Run docker compose ps to check which resources are running.${NC}"
-        exit -1 
-    fi
+    echo -e "[?] ${BIYellow}Looks like this script was run previously to setup Mantis.${NC}\n"
+    sudo docker compose ps --format json | jq -r \
+    '["Service","Status"], ["--------","------------"], (.[] | [.Service, .State]) | @tsv' | column -ts $'\t'
+
+echo -e -n "
+[!] ${Yellow}Previously created resources need to be cleaned up before proceeding with installation \n
+
+Following are your options:
+
+"
+
+echo -e "${BICyan}
+1. Delete all the previously created containers${BYellow}
+    - Recommended to export the dashboard before deleting Appsmith
+    - ${BYellow}Recommended to backup the database before deleting MongoDB${BICyan}
+2. Delete Mantis, Appsmith and don't delete MongoDB
+    - ${BYellow}Recommended to export the dashboard before deleting Appsmith${BICyan}
+3. Delete Mantis, MongoDB and don't delete Appsmith
+    - ${BYellow}Recommended to backup the database before deleting MongoDB 
+      https://www.mongodb.com/docs/manual/tutorial/backup-and-restore-tools/${BICyan}
+4. Delete Mantis and don't delete Appsmith, MongoDB
+"
+
+read -p "What would you like to do? (1/2/3/4): " choice
+
+case $choice in
+    1)
+        echo -e "[-] ${Red}Removing all the existing containers from Mantis setup${NC}"
+        sudo docker compose down
+        ;;
+    2)
+        echo -e "[-] ${Red}Removing Mantis, Appsmith and retaining MongoDB${NC}"
+        sudo docker compose down appsmith
+        sudo docker compose down mantis
+        ;;
+    3)
+        echo -e "[-] ${Red}Removing Mantis, MongoDB and retaining Appsmith${NC}"
+        sudo docker compose down mongodb
+        sudo docker compose down mantis
+        ;;
+
+    4)
+        echo -e "[-] ${Red}Removing Mantis and retaining MongoDB, Appsmith${NC}"
+        sudo docker compose down mantis
+        ;;
+    *)
+        echo -e "\nInvalid choice. Please select a valid option (1/2/3/4)."
+        ;;
+esac
+
 fi
 
 ## Setup aliases
@@ -97,7 +133,7 @@ COMMAND_CONTENT="docker exec -it mantis bash"
 
 # Check if the command already exists
 
-echo -e "[*] ${BPurple}Sudo is only required to provide a seamless user experience by adding aliases on host system ${NC}"
+echo -e "[*] ${BPurple}Sudo is required for running docker and adding aliases/commands on host system${NC}"
 echo -e -n "[?] ${BICyan}Do you have sudo access on the machine? (y/n)? ${NC}"
     read sudo_answer
     if [ "$sudo_answer" != "${sudo_answer#[Yy]}" ] ;then 
@@ -111,35 +147,48 @@ echo -e -n "[?] ${BICyan}Do you have sudo access on the machine? (y/n)? ${NC}"
 
 sudo apt update && sudo apt install jq -y
 
-docker compose up --remove-orphans -d
+sudo docker compose up --remove-orphans -d --build
 
 echo -e "${BIYellow}\n\nSETUP SUMMARY${NC}\n"
 
-docker compose ps --format json | jq -r \
+sudo docker compose ps --format json | jq -r \
 '["Service","Status"], ["--------","------------"], (.[] | [.Service, .State]) | @tsv' | column -ts $'\t'
 
 sudo  sed -i "/mantis/d" /etc/hosts
 sudo -- sh -c -e "echo '10.10.0.3  mantis.db' >> /etc/hosts";
 sudo -- sh -c -e "echo '127.0.0.1  mantis.dashboard' >> /etc/hosts";
 
+appsmith_exists=$(docker compose ps appsmith --quiet)
+mongo_exists=$(docker compose ps mongodb --quiet)
+mantis_exists=$(docker compose ps mantis --quiet)
+
+if [ -n "$appsmith_exists" ] && [ -n "$mongo_exists" ] && [ -n "$mantis_exists" ]; then
 echo -e -n "
 ${BIGreen}
     Mantis has been setup successfully on docker!
 
-    1. You can access Mantis container using: ${BICyan}docker exec -it mantis bash${BIGreen}
+    1. You can find the Mantis shell below where you can run mantis commands. Run help for further instructions.
+        - You can always access Mantis container again using: ${BICyan}docker exec -it mantis bash${BIGreen}
         - For ease of use, run ${BICyan}mantis-activate${BIGreen} command anywhere on the system to exec into Mantis docker
    
-    2. Appsmith dashboard application is accessible on the host's localhost port 1337
+    2. Mantis dashboard (appsmith) is accessible on the host's localhost port 1337
         - For ease of use, you can access dashboard from your system at ${BICyan}http://mantis.dashboard:1337${BIGreen}
-        - You can access Appsmith container using - ${BICyan}docker exec -it appsmith bash${BIGreen}
+        - You can access appsmith container using - ${BICyan}docker exec -it appsmith bash${BIGreen}
         - Configure your dashboard using instructions at https://
 
     3. You can access MongoDB container using: ${BICyan}docker exec -it mongodb bash${BIGreen}
 
-    4. Command "mantis-activate" is added to your local system. Run this from anywhere to drop into Mantis shell
-    5. Mantis documentation is available at https://phonepe.github.io/mantis
-    6. Get help and give feedback at https://slack.com
+    4. Mantis documentation is available at https://phonepe.github.io/mantis
+    5. Get help and give feedback at https://slack.com
     ${NC}
 "
+else
+    echo -e -n "
+${BIRed}
+Mantis has NOT setup successfully on docker!
+
+Please check the container/service status above and check for any errors to resolve this issue.
+"
+fi
 
 docker exec -it mantis bash
